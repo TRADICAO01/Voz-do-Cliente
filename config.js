82c354176c31d42f11535fe8c673fe7214d08f76
TRADICAO01 <supermercadotradicao.01@gmail.com>
2026-09-20 15:32:48 -0300

    Add configuration settings to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.TRADICAO_CONFIG = {
+  SUPABASE_URL: "https://bdlzvjksuutaebeqophb.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_I1g-Cg1CtQO_O4q-9PzsVw_bfKj-avn",
+  WHATSAPP_GROUP_URL: "",
+  GOOGLE_REVIEW_URL: ""
+};
