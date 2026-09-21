843995e2dcc2f433788296447aa14fc95123fa93
TRADICAO01 <supermercadotradicao.01@gmail.com>
2026-09-21 11:16:58 -0300

    Update GOOGLE_REVIEW_URL in config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.TRADICAO_CONFIG = {
   SUPABASE_URL: "https://bdlzvjksuutaebeqophb.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_I1g-Cg1CtQO_O4q-9PzsVw_bfKj-avn",
   WHATSAPP_GROUP_URL: "https://chat.whatsapp.com/DkJkHitF7vDC9uoqmhhU8H?mode=gi_t",
-  GOOGLE_REVIEW_URL: ""
+  GOOGLE_REVIEW_URL: "https://www.google.com/maps/place/Supermercado+Tradi%C3%A7%C3%A3o/@-23.7748505,-53.3335625,17z/data=!4m8!3m7!1s0x94f2d700010f9a85:0x3e610202034272a0!8m2!3d-23.774766!4d-53.3309264!9m1!1b1!16s%2Fg%2F11yylf621r?entry=ttu&g_ep=EgoyMDI2MDkxNi4wIKXMDSoASAFQAw%3D%3D",
 };
